fca1da047bdf6de58f8d4fe4c5bd5953c44af290
danny serra <danielserradev@gmail.com>
2019-10-08 11:50:22 -0500

    wrote the 4 add tests, now working on the add method

--- CustomListProject/CustomList.cs
+++ CustomListProject/CustomList.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace CustomListProject
 {
-    class CustomList
+    public class CustomList<T>
     {
+        private T[] items;
+
+        public CustomList()
+        {
+            items = new T[4];
+        }
+        
+        public T this[int i]
+        {
+            get { return items[i]; }
+            set { items[i] = value; }
+        }
+        public void Add(T itemToAdd)
+        {
+         
+            CustomList<T> customList = new CustomList<T>();
+            for (int i = 0; i < items.Length; i++)
+            {
+                customList[i] = itemToAdd;
+            }
+
+        }
+
     }
 }
--- UnitTestProject/UnitTest1.cs
+++ UnitTestProject/UnitTest1.cs
@@ -1,14 +1,84 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using CustomListProject;
 namespace UnitTestProject
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Example()
         {
+            //Arrange
+
+            //Assemble
+
+            //Assert
+
+        }
+        [TestMethod]
+        public void Add_AddToEmptyList_PutAtIndexZero()
+        {
+            //Arrange
+            CustomList<int> testList = new CustomList<int>();
+            int expected = 1;
+            int actual;
+
+            //Assemble
+            testList.Add(1);
+            actual = testList[0];
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Add_AddToList_CountLength()
+        {
+            //Arrange
+            CustomList<int> testList = new CustomList<int>();
+            int expected = 1;
+            int actual;
+
+            //Assemble
+            testList.Add(123);
+            actual = testList.Count;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Add_AddMiltipleItems_FillList()
+        {
+            //Arrange
+            CustomList<int> testList = new CustomList<int>();
+            int expected = 10;
+            int actual;
+
+            //Assemble
+            for(int i = 0; i < 10; i++)
+            {
+                testList.Add(1);
+            }
+            actual = testList.Count;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Add_AddMultipleItemsToEmptyList_FillFromIndexZeroOn()
+        {
+            //Arrange
+            CustomList<int> testList = new CustomList<int>();
+            int expected = 2;
+            int actual;
+
+            //Assemble
+            testList.Add(1);
+            testList.Add(2);
+            actual = testList[1];
+
+            //Assert
+            Assert.AreEqual(expected, actual);
         }
     }
 }
